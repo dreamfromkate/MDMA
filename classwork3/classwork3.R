23d4d2ff2a8e5f8f8070a4197198f353570efd77
dreamfromkate <musia4040@ya.ru>
2018-10-04 11:28:44 +0500

    _

---
+++ classwork3/classwork3.R
@@ -0,0 +1,8 @@
+#Загрузите данные о землятресениях
+anss <- readLines("https://raw.githubusercontent.com/SergeyMirvoda/MD-DA-2017/master/data/earthquakes_2011.html", warn=FALSE)
+
+#Выберите строки, которые содержат данные с помощью регулярных выражений и функции grep
+data<-strsplit(anss[grep(pattern="^[^<.>]", anss)], ",")
+
+#Проверьте что все строки (all.equal) в результирующем векторе подходят под шаблон. 
+all.equal(data, grep(pattern="^[^<.>]", anss))
